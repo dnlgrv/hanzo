e7dc1688c84db77129b3cb5c4334167765d3f404
Daniel Grieve <git@cazrin.net>
2016-01-27 15:34:20 +0000

    Add ParseClient dependency

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,10 @@ use Mix.Config
 
 # config :hanzo, Hanzo.Slack,
 #   token: "BOT TOKEN"
+#
+# config :parse_client,
+#   parse_application_id: "PARSE APPLICATION ID",
+#   parse_api_key: "PARSE API KEY"
 
 config :logger, :console,
   format: "$time [$level] $message\n\n"
--- mix.exs
+++ mix.exs
@@ -11,14 +11,15 @@ defmodule Hanzo.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :slack, :httpoison],
+    [applications: [:logger, :slack, :httpoison, :parse_client],
      mod: {Hanzo, []}]
   end
 
   defp deps do
-    [{:slack, "~> 0.4.1"},
+    [{:slack, git: "https://github.com/cazrin/Elixir-Slack.git", branch: "relaxed-http-poison-version"},
      {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git"},
-     {:httpoison, "~> 0.8.1"},
-     {:exjsx, "~> 3.1.0"}]
+     {:httpoison, ">= 0.7.0", override: true},
+     {:exjsx, "~> 3.1.0"},
+     {:parse_client, git: "https://github.com/cazrin/parse_elixir_client", branch: "no-bang-atoms"}]
   end
 end
